581eebeae144fffaa3d7204690bdc13d39b65948
Bhanu pratap singh <pbhanu314@gmail.com>
2020-12-21 10:56:49 +0530

    Container Changes done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import Album from './Components/Album'
 import Header from './Components/Header'
 import Grids from "./Components/Grids"
 import Paper from '@material-ui/core/Paper'
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, Typography } from '@material-ui/core';
 import Container from '@material-ui/core/Container'
 
 const useStyle = makeStyles((theme) => ({
   root: {
     flexGrow: 1
   },
+  container: {
+    // background: "red",
+    marginTop: 10
+  }
 }));
 
 function App() {
@@ -16,7 +20,7 @@ function App() {
   return (
     <div className="App" className={classes.root} >
       <Header></Header>
-      <Container maxWidth="md">
+      <Container maxWidth="md" className={classes.container}>
         <Grids></Grids>
       </Container>
 
